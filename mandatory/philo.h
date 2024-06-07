d79ffd4c60b3090e541d7d25175eb962108ca7e4
Soufiane Essarhir <soufianeessarhir@gmail.com>
2024-06-07 04:40:58 +0100

    on fixing proplems

--- mandatory/philo.h
+++ mandatory/philo.h
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 20:23:53 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/06/06 05:58:48 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/06/07 04:19:09 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,34 +30,40 @@
 #define BOLD    "\033[1m"
 #define UNDERLINE "\033[4m"
 #define REVERSE "\033[7m"
+
+typedef struct s_data t_data;
 typedef struct s_philo
 {
-	pthread_t		admin;
-	pthread_t		thread;
-	size_t			last_time_eat;
-	int				id;
-	size_t			start_time;
-	size_t			time_to_die;
-	size_t			time_to_eat;
-	size_t			time_to_sleep;
-	int				num_of_philos;
-	int				num_times_to_eat;
-	pthread_mutex_t	*left_fork;
-	pthread_mutex_t	*right_fork;
-}					t_philo;
+    int             dead;
+    pthread_t       thread;
+    size_t          last_time_eat;
+    int             id;
+    size_t          time_to_eat;
+    size_t          time_to_sleep;
+    int             num_times_to_eat;
+    pthread_mutex_t *left_fork;
+    pthread_mutex_t *right_fork;
+    t_data          *data;
+} t_philo;
 
-typedef struct data
+typedef struct s_data
 {
-	t_philo			*philo;
-	pthread_mutex_t	*forks;
-}t_data;
+    int             dead_flag;
+    size_t          start_time;
+    int             num_of_philos;
+    pthread_t       admin;
+    size_t          time_to_die;
+    t_philo         *philo;
+    pthread_mutex_t *forks;
+    pthread_mutex_t dead_flag_mutex;
+} t_data;
 
 
 int ft_parce_args(int ac, char **av);
 int	ft_atoi(const char *str);
 void *s_malloc (size_t size);
 size_t	current_time(void);
-int	ft_usleep(size_t t_ms);
+int	ft_usleep(size_t t_ms,t_philo *philo);
 void init_data(t_data *data, int ac, char **av);
 
 #endif
--- mandatory/utils.c
+++ mandatory/utils.c
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 18:34:35 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/06/06 05:57:57 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/06/07 04:21:00 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,34 +51,47 @@ size_t	current_time(void)
 	gettimeofday(&time, NULL);
 	return (time.tv_sec * 1e3 + time.tv_usec / 1e3);
 }
-int	ft_usleep(size_t t_ms)
+int	ft_usleep(size_t t_ms,t_philo *philo)
 {
 	size_t	start;
 
 	start = current_time();
 	while ((current_time() - start) < t_ms)
+	{
+		pthread_mutex_lock(&philo->data->dead_flag_mutex);
+		if (philo->data->dead_flag)
+		{
+			pthread_mutex_unlock(&philo->data->dead_flag_mutex);
+			break;
+		}
+		pthread_mutex_unlock(&philo->data->dead_flag_mutex);
 		usleep(100);
+	}
 	return (0);
 }
 void init_data(t_data *data, int ac, char **av)
 {
     int i;
     int num_philos;
-	
-	i = 0;
-	num_philos = atoi(av[1]);
+
+    i = 0;
+    num_philos = atoi(av[1]);
     data->philo = malloc(sizeof(t_philo) * num_philos);
     data->forks = malloc(sizeof(pthread_mutex_t) * num_philos);
+    data->num_of_philos = num_philos;
+    data->time_to_die = atoi(av[2]);
+    pthread_mutex_init(&data->dead_flag_mutex, NULL);
+
+    data->dead_flag = 0;
     while (i < num_philos)
     {
         data->philo[i].id = i + 1;
-        data->philo[i].num_of_philos = num_philos;
-        data->philo[i].time_to_die = atoi(av[2]);
+        data->philo[i].num_times_to_eat = (ac == 6) ? atoi(av[5]) : -1;
         data->philo[i].time_to_eat = atoi(av[3]);
         data->philo[i].time_to_sleep = atoi(av[4]);
-        data->philo[i].num_times_to_eat = (ac == 6) ? atoi(av[5]) : -1;
         data->philo[i].left_fork = &data->forks[i];
         data->philo[i].right_fork = &data->forks[(i + 1) % num_philos];
+        data->philo[i].data = data;
         pthread_mutex_init(&data->forks[i], NULL);
         i++;
     }
