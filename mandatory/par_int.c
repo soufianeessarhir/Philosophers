e50343e531c79024f6ec16747691e4d3d4f3f854
Soufiane Essarhir <soufianeessarhir@gmail.com>
2024-06-08 07:17:40 +0100

    on handling

--- mandatory/par_int.c
+++ mandatory/par_int.c
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 14:26:14 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/06/08 00:14:26 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/06/08 07:05:05 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,7 @@ void init_data(t_data *data, int ac, char **av)
         	data->philo[data->counter].num_times_to_eat = ft_atoi(av[5]);
 		else
         	data->philo[data->counter].num_times_to_eat =  -1;
+		data->philo->last_time_eat = current_time();
         data->philo[data->counter].time_to_eat = ft_atoi(av[3]);
         data->philo[data->counter].time_to_sleep = ft_atoi(av[4]);
         data->philo[data->counter].left_fork = &data->forks[data->counter];
