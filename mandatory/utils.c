d49f8f8ac11476834bedac61594ec031b5942e1b
Soufiane Essarhir <soufianeessarhir@gmail.com>
2024-06-12 16:58:28 +0100

    i add some fixing :)

--- mandatory/utils.c
+++ mandatory/utils.c
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 18:34:35 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/06/11 08:12:51 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/06/12 16:55:48 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,6 @@ int	ft_atoi(const char *str)
 	return (s * nb);
 }
 
-void *s_malloc (size_t size)
-{
-	void *val;
-	val = malloc(size);
-	if (!val)
-		exit(printf("malloc has been failed when it tries to alloc %zu\n size",size));
-	else
-		return val;	
-}
-
 size_t	current_time(void)
 {
 	struct timeval	time;
@@ -51,15 +41,38 @@ size_t	current_time(void)
 	gettimeofday(&time, NULL);
 	return (time.tv_sec * 1e3 + time.tv_usec / 1e3);
 }
-int	ft_usleep(size_t t_ms)
+
+void ft_message(t_philo *philo, char *text, char *color)
+{
+    pthread_mutex_lock(philo->message);
+    if (!dead_check(philo))
+        printf("%s""%zu %d %s\n" RESET, color, current_time() - philo->start_time, philo->id, text);
+    pthread_mutex_unlock(philo->message);
+}
+int	ft_usleep(size_t t_ms, t_philo *philo)
 {
 	size_t	start;
 
 	start = current_time();
 	while ((current_time() - start) < t_ms)
 	{
+		if (dead_check(philo))
+			return (1);
 		usleep(100);
 	}
 	return (0);
 }
+int dead_check(t_philo *philo)
+{
+    int i;
 
+    i = -1;
+    while (++i < philo->num_of_philos)
+    {
+        pthread_mutex_lock(philo->dead_flag_mutex);
+        if (*philo->dead == 1)
+            return (pthread_mutex_unlock(philo->dead_flag_mutex),1);   
+        pthread_mutex_unlock(philo->dead_flag_mutex);
+    }
+    return 0;
+}
