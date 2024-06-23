2cd7632d9bb3a4329495845b8ca33779dc8bab63
سفيان الصغير <soufianeessarhir@gmail.com>
2024-06-24 00:41:46 +0100

    reordering the files:)

---
+++ philo/par_int.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   par_int.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 14:26:14 by sessarhi          #+#    #+#             */
+/*   Updated: 2024/06/13 11:21:32 by sessarhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static	int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
+static	int	check_dig(char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (ft_isdigit(av[i][j]) == 0)
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+int ft_parce_args(int ac, char **av)
+{
+	int i;
+
+	i = 0;
+	while (i < ac)
+	{
+		if (!av[i])
+			return (0);	
+		i++;	
+	}
+	if (!check_dig(av))
+		return 0;
+	return (1);
+}
+void init_data(t_data *data, int ac, char **av)
+{
+    data->counter = -1;
+    data->num_of_philos = ft_atoi(av[1]);
+    data->time_to_die = ft_atoi(av[2]);
+    data->philo = malloc(sizeof(t_philo) * data->num_of_philos);
+    data->forks = malloc(sizeof(pthread_mutex_t) * data->num_of_philos);
+    data->dead_flag = 0;
+	pthread_mutex_init(&data->time_mutex, NULL);
+	pthread_mutex_init(&data->message,NULL);
+    pthread_mutex_init(&data->dead_flag_mutex, NULL);
+    while (++data->counter < data->num_of_philos)
+    {
+        pthread_mutex_init(&data->forks[data->counter], NULL);
+        data->philo[data->counter].id = data->counter + 1;
+		if (ac == 6)
+        	data->philo[data->counter].num_times_to_eat = ft_atoi(av[5]);
+		else
+        	data->philo[data->counter].num_times_to_eat =  -1;
+		data->philo[data->counter].num_times_eaten = 0;
+		data->philo[data->counter].dead_flag_mutex = &data->dead_flag_mutex;
+		data->philo[data->counter].time_mutex = &data->time_mutex;
+		data->philo[data->counter].message = &data->message;
+		data->philo[data->counter].dead = &data->dead_flag;
+		data->philo[data->counter].num_of_philos = data->num_of_philos;
+        data->philo[data->counter].time_to_eat = ft_atoi(av[3]);
+        data->philo[data->counter].time_to_sleep = ft_atoi(av[4]);
+		data->philo[data->counter].start_time = current_time();
+		data->philo[data->counter].last_time_eat = current_time();
+        data->philo[data->counter].left_fork = &data->forks[data->counter];
+        data->philo[data->counter].right_fork = &data->forks[(data->counter + 1) % data->num_of_philos];
+    }
+}
+
---
+++ philo/utils.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/19 18:34:35 by sessarhi          #+#    #+#             */
+/*   Updated: 2024/06/12 16:55:48 by sessarhi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+int	ft_atoi(const char *str)
+{
+	long long	nb;
+	int			s;
+
+	nb = 0;
+	s = 1;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			s = -1;
+		str++;
+	}
+	while (*str && (*str >= '0' && *str <= '9'))
+	{
+		nb = nb * 10 + (*str - '0');
+		str++;
+	}
+	return (s * nb);
+}
+
+size_t	current_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1e3 + time.tv_usec / 1e3);
+}
+
+void ft_message(t_philo *philo, char *text, char *color)
+{
+    pthread_mutex_lock(philo->message);
+    if (!dead_check(philo))
+        printf("%s""%zu %d %s\n" RESET, color, current_time() - philo->start_time, philo->id, text);
+    pthread_mutex_unlock(philo->message);
+}
+int	ft_usleep(size_t t_ms, t_philo *philo)
+{
+	size_t	start;
+
+	start = current_time();
+	while ((current_time() - start) < t_ms)
+	{
+		if (dead_check(philo))
+			return (1);
+		usleep(100);
+	}
+	return (0);
+}
+int dead_check(t_philo *philo)
+{
+    int i;
+
+    i = -1;
+    while (++i < philo->num_of_philos)
+    {
+        pthread_mutex_lock(philo->dead_flag_mutex);
+        if (*philo->dead == 1)
+            return (pthread_mutex_unlock(philo->dead_flag_mutex),1);   
+        pthread_mutex_unlock(philo->dead_flag_mutex);
+    }
+    return 0;
+}
