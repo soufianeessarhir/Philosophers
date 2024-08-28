b3ba40cd407e1f59c0627f862f7badd9546b3265
Soufiane Essarhir <soufianeessarhir@gmail.com>
2024-08-28 17:54:24 +0100

    fixing

---  philo_bonus/par_int_bonus.c
+++  philo_bonus/par_int_bonus.c
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 14:26:14 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/08/27 17:01:43 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/08/28 17:30:52 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ static	int	ft_isdigit(int c)
 		return (1);
 	return (0);
 }
+
 static	int	check_dig(char **av)
 {
 	int	i;
@@ -37,6 +38,7 @@ static	int	check_dig(char **av)
 	}
 	return (1);
 }
+
 int ft_parce_args(int ac, char **av)
 {
 	int i;
@@ -46,30 +48,33 @@ int ft_parce_args(int ac, char **av)
 	{
 		if (!av[i])
 			return (0);	
-		i++;	
+		i++;
 	}
 	if (!check_dig(av))
 		return 0;
 	return (1);
 }
+
 void init_philo(t_philo *philo, int ac, char **av)
 {
 	int i;
 
 	i = -1;
-    philo->num_of_philos = ft_atoi(av[1]);
-    philo->time_to_die = ft_atoi(av[2]);
+	philo->num_of_philos = ft_atoi(av[1]);
+	philo->time_to_die = ft_atoi(av[2]);
 	philo->eat = malloc(sizeof(sem_t *) * philo->num_of_philos);
 	if (!philo->eat)
-		return (exit(printf(RED"Error malloc\n"RESET)),(void)0);
+		return (exit(printf(RED"Error malloc\n"RESET)), (void)0);
 	sem_unlink("fork_sem");
 	sem_unlink("dead_sem");
 	sem_unlink("message_sem");
-    philo->fork = sem_open("fork_sem", O_CREAT, 0644, philo->num_of_philos);
-    philo->message = sem_open("message_sem", O_CREAT, 0644, 1);
+	philo->fork = sem_open("fork_sem", O_CREAT, 0644, philo->num_of_philos);
+	philo->message = sem_open("message_sem", O_CREAT, 0644, 1);
 	philo->dead = sem_open("dead_sem", O_CREAT, 0644, 1);
-	if (philo->fork == SEM_FAILED || philo->dead == SEM_FAILED || philo->message == SEM_FAILED)
-        return (free(philo->eat), exit(printf(RED"Error sem_open\n"RESET)),(void)0);
+	if (philo->fork == SEM_FAILED || philo->dead == SEM_FAILED
+		|| philo->message == SEM_FAILED)
+		return (free(philo->eat),
+			exit(printf(RED"Error sem_open\n"RESET)), (void)0);
 	philo->time_to_eat = ft_atoi(av[3]);
 	philo->time_to_sleep = ft_atoi(av[4]);
 	if (ac == 6)
@@ -77,4 +82,3 @@ void init_philo(t_philo *philo, int ac, char **av)
 	else
 		philo->num_times_to_eat = -1;
 }
-
---  philo_bonus/philo_bonus.h
+++  philo_bonus/philo_bonus.h
@@ -6,63 +6,68 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/12 17:14:17 by sessarhi          #+#    #+#             */
-/*   Updated: 2024/08/28 14:58:49 by sessarhi         ###   ########.fr       */
+/*   Updated: 2024/08/28 17:43:14 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #ifndef PHILO_BONUS_H
-#define PHILO_BONUS_H
+# define PHILO_BONUS_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <sys/time.h>
-#include <signal.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <semaphore.h>
+# include <sys/time.h>
+# include <signal.h>
 
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN    "\033[36m"
-#define WHITE   "\033[37m"
-#define BOLD    "\033[1m"
-#define UNDERLINE "\033[4m"
-#define REVERSE "\033[7m"
-#define BOLD_RED "\x1b[1m\x1b[31m"
+# define RESET	"\033[0m"
+# define RED     "\033[31m"
+# define GREEN   "\033[32m"
+# define YELLOW  "\033[33m"
+# define BLUE    "\033[34m"
+# define MAGENTA "\033[35m"
+# define CYAN    "\033[36m"
+# define WHITE   "\033[37m"
+# define BOLD    "\033[1m"
+# define UNDERLINE "\033[4m"
+# define REVERSE "\033[7m"
+# define BOLD_RED "\x1b[1m\x1b[31m"
 
 typedef struct s_philo
 {
-	int             num_of_philos;
-    pthread_t       philo;
-    size_t          last_time_eat;
-    int             id;
-    size_t          time_to_eat;
-    size_t          time_to_sleep;
-    int             num_times_eaten;
-    int             num_times_to_eat;
-    size_t          start_time;
-	size_t          time_to_die;
+	int				num_of_philos;
+	pthread_t		philo;
+	size_t			last_time_eat;
+	int				id;
+	size_t			time_to_eat;
+	size_t			time_to_sleep;
+	int				num_times_eaten;
+	int				num_times_to_eat;
+	size_t			start_time;
+	size_t			time_to_die;
 	sem_t			*fork;
 	sem_t			**eat;
 	sem_t			*message;
-    sem_t			*dead;
+	sem_t			*dead;
 	char			*str;
-} t_philo;
+}	t_philo;
 
-
-int ft_parce_args(int ac, char **av);
-int	ft_atoi(const char *str);
+int		ft_parce_args(int ac, char **av);
+int		ft_atoi(const char *str);
 size_t	current_time(void);
-int	ft_usleep(size_t t_ms, t_philo *philo);
+int		ft_usleep(size_t t_ms, t_philo *philo);
 char	*ft_itoa(int n);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *s);
-// int dead_check(t_philo *philo);
-void ft_message(t_philo *philo, char *text, char *color);
-void init_philo(t_philo *philo, int ac, char **av);
+void	ft_message(t_philo *philo, char *text, char *color);
+void	init_philo(t_philo *philo, int ac, char **av);
+int		parent_process(t_philo *philo ,int *pid);
+int		child_process(t_philo *philo);
+void	free_all(t_philo *philo);
+int		th_starting(t_philo *philo);
+void	*worker(void *args);
+void	f(void);
+void	*a_worker(void *args);
+
 #endif
